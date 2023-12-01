ab5d308e694a7d8fddb3cb5adbdb2c2facd2fa15
lukehcjackson <luke.hc.jackson@gmail.com>
2023-12-01 00:13:23 +0000

    Improved weather conditions storage and added checks for all weather types
    Used objects to store info about each weather variable. Added 1000 if else statements to check them because there didnt seem like a better option frankly.

--- app.js
+++ app.js
@@ -35,42 +35,120 @@ function callDataPointAPI() {
 //takes a Rep object as input and evaluates each metric and gives it a green/amber/red rating
 function evaluateDataPoint(rep) {
 
-  const speed = parseInt(rep["S"]); 
-  const gust = parseInt(rep["G"]);
   const weatherType = rep["W"];
-  const temp = parseInt(rep["F"]);
-  const visibility = rep["V"];
 
-  /*
-  Better way of doing this:
-  struct for each variable, holding the data, and each threshold value
-  i.e for speed:
-  speed {
-    data = parseInt(rep["S"]);
-    red = 35;
-    amber = 24;
-    green not even necessary in this case
+  const speed = {
+    data: parseInt(rep["S"]),
+    red: 35,
+    amber: 24,
+    green: 0
   }
 
-  then do speed.data > speed.red -> output red
-  */
-
-  //evaluate speed
-  if (speed > 35) {
-    console.log("Wind Speed Red: " + speed.toString());
-  } else if (speed >= 24) {
-    console.log("Wind Speed Amber: " + speed.toString());
-  } else {
-    console.log("Wind Speed Green: " + speed.toString());
+  const gust = {
+    data: parseInt(rep["G"]),
+    red: 40,
+    amber: 30,
+    green: 0
   }
 
-  //evaluate gust
-  if (gust > 40) {
-    console.log("Wind Gust Red: " + gust.toString());
-  } else if (gust >= 30) {
-    console.log("Wind Gust Amber: " + gust.toString());
+  const temp = {
+    data: parseInt(rep["F"]),
+    redHigh: 40,
+    redLow: -8,
+    amberHigh: 30,
+    amberLow: -6, //must specifically check for -7 and -8c as some boats CAN go out
+    green: 0
+  }
+
+  const rain = {
+    data: weatherType.toString(),
+    red: "13",
+    amber: "12"
+  }
+
+  const snow = {
+    data: weatherType.toString(),
+    red: "27",
+    amber: "24"
+  }
+
+  const electricalStorms = {
+    data: weatherType.toString(),
+    red: ["W29", "W30"]
+  }
+
+  const visibility = {
+    data: rep["V"].toString(),
+    red: "VP",
+    amber: "PO"
+  }
+
+  //WIND SPEED
+  if (speed.data > speed.red) {
+    console.log("RED WIND SPEED");
+  } else if (speed.data > speed.amber) {
+    console.log("AMBER WIND SPEED");
   } else {
-    console.log("Wind Gust Green: " + gust.toString());
+    console.log("GREEN WIND SPEED");
+  }
+
+  //WIND GUST
+  if (gust.data > gust.red) {
+    console.log("RED WIND GUST");
+  } else if (gust.data > gust.amber) {
+    console.log("AMBER WIND GUST");
+  } else {
+    console.log("GREEN WIND GUST");
+  }
+
+  //TEMPERATURE
+  if (temp.data > temp.redHigh) {
+    console.log("RED TEMPERATURE (HIGH");
+  } else if (temp.data > temp.amberHigh) {
+    console.log("AMBER TEMPERATURE (HIGH");
+  } else if (temp.data >= temp.green) {
+    console.log("GREEN TEMPERATURE");
+  } else if (temp.data > temp.amberLow) {
+    console.log("AMBER TEMPERATURE (LOW)");
+  } else if (temp.data >= temp.redLow) {
+    console.log("AMBER TEMPERATURE (LOW) - EXPERIENCED COXLESS BOATS ONLY");
+  } else {
+    console.log("RED TEMPERATURE (LOW)");
+  }
+
+  //WEATHER TYPE
+  //RAIN
+  if (rain.data == rain.red) {
+    console.log("RED RAIN");
+  } else if (rain.data == rain.amber) {
+    console.log("AMBER RAIN");
+  } else {
+    console.log("GREEN RAIN");
+  }
+
+  //SNOW
+  if (snow.data == snow.red) {
+    console.log("RED SNOW");
+  } else if (snow.data == snow.amber) {
+    console.log("AMBER SNOW");
+  } else {
+    console.log("GREEN SNOW");
+  }
+
+  //ELECTRICAL STORMS
+  if (electricalStorms.red.includes(electricalStorms.data)) {
+    console.log("RED ELECTRICAL STORMS");
+  } else {
+    console.log("GREEN ELECTRICAL STORMS");
+  }
+
+  //VISIBILITY
+  if (visibility.data == visibility.red) {
+    console.log("RED VISIBILITY");
+  } else if (visibility.data == visibility.amber) {
+    console.log("AMBER VISIBILITY");
+  } else {
+    console.log("GREEN VISIBILITY");
   }
 
 }
